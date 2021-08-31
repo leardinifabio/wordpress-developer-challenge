2ac34a89e9677d463915611e2635984f9de33942
Fabio Leardini <leardinifabio90@gmail.com>
2021-08-31 00:44:58 -0300

    pagina filmes att

--- desafio-wp/footer.php
+++ desafio-wp/footer.php
@@ -1,3 +1,13 @@
+<footer class="footer-play">
+    <div class="footer-logo">
+        <?php
+        the_custom_logo();
+        ?>
+    </div>
+    <p class="footer-text-area">&copy; <?= date("Y") ?> - Play - Todos os direitos reservados.</p>
+
+</footer>
+
 </body>
 <?php wp_footer(); ?>
 
